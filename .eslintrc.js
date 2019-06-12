65295104b8f427da0fdcb90aa9ad8f2da407d474
xaviguasch <xaviguasch@gmail.com>
2019-06-12 12:52:06 +0200

    added client folders and connected them with Concurrenty

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,18 +3,29 @@ module.exports = {
     browser: true,
     es6: true
   },
-  extends: 'eslint:recommended',
+  extends: ['eslint:recommended', 'plugin:react/recommended'],
   globals: {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly'
   },
   parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
     ecmaVersion: 2018,
     sourceType: 'module'
   },
+  plugins: ['react'],
   rules: {
     'no-console': 'off',
-    'no-unused-vars': 'off',
-    'no-undef': 'off'
+    'react/prop-types': 0,
+    'react/no-unescaped-entities': 0
+  },
+  parser: 'babel-eslint',
+  settings: {
+    react: {
+      pragma: 'React',
+      version: '16.5.2'
+    }
   }
 }
