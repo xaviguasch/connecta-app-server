512c4a4b2282a29e63fe67ab5b7cdbb728a049bc
xaviguasch <xaviguasch@gmail.com>
2019-06-14 16:18:59 +0200

    edited and updated the contacts

--- client/src/context/contact/contactReducer.js
+++ client/src/context/contact/contactReducer.js
@@ -15,6 +15,13 @@ export default (state, action) => {
         ...state,
         contacts: [...state.contacts, action.payload]
       }
+    case UPDATE_CONTACT:
+      return {
+        ...state,
+        contacts: state.contacts.map(contact =>
+          contact.id === action.payload.id ? action.payload : contact
+        )
+      }
     case DELETE_CONTACT:
       return {
         ...state,
@@ -30,6 +37,7 @@ export default (state, action) => {
         ...state,
         current: null
       }
+
     default:
       return state
   }
